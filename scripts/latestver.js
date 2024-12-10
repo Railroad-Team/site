7b317b3314362c9bf300ccadceb50f39f98dd627
YodaForce157 <69751882+YodaForce157@users.noreply.github.com>
2024-12-10 15:32:06 +0000

    Update latestver.js

--- scripts/latestver.js
+++ scripts/latestver.js
@@ -1,6 +1,20 @@
-function getLatestVer(){
-    let ver = '1.0.0';
-    //Todo: Find where https://www.github.com/Railroad-team/Railroad/releases/latest redirects to & then get the version
-    //From the end of the url
+async function getRedirectedUrl(url) {
+    try {
+        const response = await fetch(url, {
+            method: 'HEAD',
+            redirect: 'follow',
+        });
+
+        console.log(`Redirected to: ${response.url}`);
+        return response.url;
+    } catch (error) {
+        console.error(`Failed to fetch the URL: ${error}`);
+    }
+}
+
+async function getLatestVer(){
+    const redirected = await getRedirectedUrl("https://www.github.com/Railroad-team/Railroad/releases/latest")
+    let ver = redirected.split("https://www.github.com/Railroad-team/Railroad/releases/")[1]
+    
     document.getElementById('latestver').innerHTML = 'Version: ' + ver;
 }
